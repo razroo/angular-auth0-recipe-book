cdb9843467a15baf2f1b69b237c6d51f458e337b
Charlie Greenman <charlie@razroo.com>
2023-09-15 03:22:06 -0400

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/angular-auth0-recipe-book/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-auth0-recipe-book-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-auth0-recipe-book';
+}
